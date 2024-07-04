157a47f9ebfcd74d726ef7140278c7f263cf969f
Harshith <harshith2742@gmail.com>
2024-07-05 01:48:33 +0530

    updated url

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://chat-app-1-gq5y.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
